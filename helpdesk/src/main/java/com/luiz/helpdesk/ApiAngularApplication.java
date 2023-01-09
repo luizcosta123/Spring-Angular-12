71e18fc07cb9eccd49908b65342377adbf4e1124
Luiz Afonso <luizcosta12322@yahoo.com>
2023-01-09 14:16:30 -0300

    First Commit

---
+++ helpdesk/src/main/java/com/luiz/helpdesk/ApiAngularApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAngularApplication.class, args);
+	}
+
+}
---
+++ helpdesk/src/test/java/com/luiz/helpdesk/ApiAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
